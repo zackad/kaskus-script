0bba21c537e57f61209cbc262566af3fb7a39a57
zackad <cahsiwarak@gmail.com>
2016-02-27 08:07:06 +0700

    styling before hiding

--- kaskus-hide-new.user.js
+++ kaskus-hide-new.user.js
@@ -3,19 +3,19 @@
 // @namespace   zackad's script
 // @description Hide Unnecessary Notification
 // @include     http://www.kaskus.co.id/*
-// @version     1.0
+// @version     1.1
 // @grant       none
 // @author      zackad
 // ==/UserScript==
+var css = '#after-login > a.notice-new::after, .site-header .dropdown-menu .notice-updated::after, .site-header .dropdown-menu .notice-new::after{'
++ 'display: none;'
++ '}'
+;
+var style = document.createElement('style');
+var t = document.createTextNode(css);
+style.appendChild(t);
+document.head.appendChild(style);
 (function () {
-  var css = '#after-login > a.notice-new::after, .site-header .dropdown-menu .notice-updated::after, .site-header .dropdown-menu .notice-new::after{'
-  + 'display: none;'
-  + '}'
-  ;
-  var style = document.createElement('style');
-  var t = document.createTextNode(css);
-  style.appendChild(t);
-  document.head.appendChild(style);
   var a = document.querySelector('.site-header .dropdown-menu > ul.list-unstyled > li.open');
   var b = a.children;
   a.classList.remove('open');
