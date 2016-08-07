91c1a1e71f1f6b0c414581cd46e2fd62d24d7632
zackad <cahsiwarak@gmail.com>
2016-08-07 10:07:41 +0700

    indentation

--- kaskus-hide-new.user.js
+++ kaskus-hide-new.user.js
@@ -1,27 +1,27 @@
 // ==UserScript==
-// @name        Kaskus Hide New
-// @namespace   zackad's script
-// @description Hide Unnecessary Notification
-// @include     http://www.kaskus.co.id/*
-// @version     1.4
-// @grant       none
-// @author      zackad
+// @name            Kaskus Hide New
+// @namespace       zackad's script
+// @description     Hide Unnecessary Notification
+// @include         http://www.kaskus.co.id/*
+// @version         1.4
+// @grant           none
+// @author          zackad
 // ==/UserScript==
 var css = '#after-login > a.notice-new::after,'
-+ '.site-header .dropdown-menu .notice-updated::after,'
-+ '.site-header .dropdown-menu .notice-new::after,'
-+ '.navbar-nav.main-menu li#kk-gadget::after,'
-+ '.navbar-nav.main-menu li#kk-news::after,'
-+ '#kk-epulsa::after, li.floating-banner'
-+ '{display: none;}'
-;
+    + '.site-header .dropdown-menu .notice-updated::after,'
+    + '.site-header .dropdown-menu .notice-new::after,'
+    + '.navbar-nav.main-menu li#kk-gadget::after,'
+    + '.navbar-nav.main-menu li#kk-news::after,'
+    + '#kk-epulsa::after, li.floating-banner'
+    + '{display: none;}'
+    ;
 var style = document.createElement('style');
 var t = document.createTextNode(css);
 style.appendChild(t);
 document.head.appendChild(style);
 (function () {
-  var a = document.querySelector('.site-header .dropdown-menu > ul.list-unstyled > li.open');
-  var b = a.children;
-  a.classList.remove('open');
-  b[1].style.display = 'none';
+    var a = document.querySelector('.site-header .dropdown-menu > ul.list-unstyled > li.open');
+    var b = a.children;
+    a.classList.remove('open');
+    b[1].style.display = 'none';
 }())
